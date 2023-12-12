f8314a95dbdbb16753560f626dd4169b5c085a50
Labh Bothra <labhbothra@Labhs-MacBook-Air.local>
2023-12-12 09:58:05 +0530

    Random Color Done

--- assests/screens/Home/HomeScreen.tsx
+++ assests/screens/Home/HomeScreen.tsx
@@ -7,6 +7,15 @@ import { useEffect, useState } from "react";
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 const HomeScreen = ({ navigation }) => {
   const isFocused = useIsFocused();
   const [allnotes, setAll] = useState([]);
@@ -26,8 +35,9 @@ const HomeScreen = ({ navigation }) => {
     const noteToEdit = allnotes[index];
     navigation.navigate("EditNotesScreen", { noteToEdit, index, allnotes, onNoteUpdate: getAllNotes });
   }
+
   const getAllNotes = async () => {
-    let x=[];
+    let x = [];
     let y = await EncryptedStorage.getItem('notes');
     let data = JSON.parse(y);
     if (data && data.data) {
@@ -36,6 +46,24 @@ const HomeScreen = ({ navigation }) => {
       });
     }
     setAll(x);
+  };
+
+  const renderItem = ({ item, index }) => {
+    const noteColor = getRandomColor();
+
+    return (
+      <TouchableOpacity onPress={() => navigateToEditScreen(index)}>
+        <View style={[styles.notetab, { backgroundColor: noteColor }]}>
+          <View style={{ width: "80%", height: "50%" }}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.desc}>{item.desc}</Text>
+          </View>
+          <TouchableOpacity style={{ height: "100%", justifyContent: 'center' }} onPress={() => deleteNote(index)}>
+            <Image style={styles.delete} source={require('../../bin.png')} />
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    );
   };
 
   return (
@@ -53,87 +81,76 @@ const HomeScreen = ({ navigation }) => {
       <View style={styles.noteback}>
         <FlatList
           data={allnotes}
-          renderItem={({ item, index }) => (
-            <TouchableOpacity onPress={() => navigateToEditScreen(index)}>
-              <View style={styles.notetab}>
-                <Text style={styles.title}>{item.title}</Text>
-                <Text style={styles.desc}>{item.desc}</Text>
-                <TouchableOpacity onPress={() => deleteNote(index)}>
-                  <Image style={styles.delete} source={require('../../delete.png')} />
-                </TouchableOpacity>
-              </View>
-            </TouchableOpacity>
-          )}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
         />
       </View>
     </View>
   );
 };
 
-
-
 const styles = StyleSheet.create({
-  delete:{
-    marginVertical:-50,
-    width:30,
-    height:40,
-    position:"absolute",
-    right:"10%"
+  delete: {
+    position: 'absolute',
+    width: 40,
+    height: 40,
   },
-  title:{
-    marginLeft:10,
-    marginTop:10,
-    fontSize:30
+  title: {
+    marginLeft: 10,
+    marginTop: 10,
+    fontSize: 30
   },
-  desc:{
-    marginLeft:10,
-    marginTop:10,
-    fontSize:20
+  desc: {
+    marginLeft: 10,
+    marginTop: 10,
+    fontSize: 20
   },
-  plusimg:{
-    width:20,
-    height:20,
-    marginLeft:10,
+  plusimg: {
+    width: 20,
+    height: 20,
+    marginLeft: 10,
   },
-  searchimg:{
-    width:20,
-    height:20,
-    marginLeft:30,
+  searchimg: {
+    width: 20,
+    height: 20,
+    marginLeft: 30,
   },
   input: {
-    width:"65%",
-    height:"60%",
+    width: "65%",
+    height: "60%",
     borderColor: 'gray',
     borderWidth: 3,
     borderRadius: 20,
-    marginLeft:10,
-    color:'white'
+    marginLeft: 10,
+    color: 'white'
   },
-  searchtab:{
-    width:"100%",
-    height:"10%",
-    marginTop:60,
-    gap:10,
-    flexDirection:"row",
-    alignItems:"center"
+  searchtab: {
+    width: "100%",
+    height: "10%",
+    marginTop: 60,
+    gap: 10,
+    flexDirection: "row",
+    alignItems: "center"
   },
-  background:{
+  background: {
     height: windowHeight,
-    width:windowWidth,
+    width: windowWidth,
     backgroundColor: 'black',
-    flexDirection:'column',
-    alignItems:'center',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-  notetab:{
-    borderRadius:20,
-    marginTop:10,
-    height:100,
-    backgroundColor:'#fcaf08',
-    width:"100%",
+  notetab: {
+    borderRadius: 20,
+    marginTop: 10,
+    flexDirection: 'row',
+    height: 100,
+    backgroundColor: '#fcaf08',
+    width: "100%",
+    gap: 20
   },
-  noteback:{
+  noteback: {
     height: windowHeight,
-    width:windowWidth,
+    width: windowWidth,
   }
 });
 
